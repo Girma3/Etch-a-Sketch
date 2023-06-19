5c761dae182d413f14cf234b714cd9f5d47e5981
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-06-19 14:03:12 +0300

    css and javascript file for etch-a-sketch

---
+++ main.js
@@ -0,0 +1,17 @@
+// function to make grid layout 
+//function makegrid(gridNumber){
+   //let gridNumber = Number(prompt("enter: "));
+   for (let i = 0; i < gridNumber * gridNumber; i++){
+
+            //grid parent with row and columns number to be set by user
+            let bigSquare = document.querySelector('.container');
+                    bigSquare.style.gridTemplateColumns = `repeat(${gridNumber} , 1fr)`;
+                    bigSquare.style.gridTemplateRows =`repeat(${gridNumber} , 1fr)`;
+
+                //grid elements created with class name
+                let miniSquares = document.createElement('tinysquares');
+                    miniSquares.setAttribute("class", "minisquares");
+                    bigSquare.appendChild(miniSquares);
+    }
+
+//}
